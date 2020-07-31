87a96ca6a45a36a180656d946ae58889ff51053d
Raffi Hovagimian <raffi.hovagimian@gmail.com>
2020-07-31 01:06:57 -0400

    Revert "Add react router & header"
    This reverts commit 2be21ba9743af8c0199f2e6ee34abb4b6caf3ccb.

--- client/index.js
+++ client/index.js
@@ -1,18 +1,9 @@
 //@ts-check
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
-import Header from "./components/Header";
 
 const Root = () => {
-  return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route path="/"></Route>
-      </Switch>
-    </Router>
-  );
+  return <div>Auth Starter</div>;
 };
 
 ReactDOM.render(<Root />, document.querySelector("#root"));
